d05061f328be2eb986e288174ba7e4ccb463b3d5
carmenavram <carmene.avram@gmail.com>
2023-12-04 08:30:35 +0100

    solve day 4

--- AdventOfCode2023/Day4.cs
+++ AdventOfCode2023/Day4.cs
@@ -13,8 +13,10 @@ internal class Day4 : IDay
     public void Solve(IList<string?> inputLines)
     {
         var sum = 0;
+        var totalCards = 0;
         int cardIndex = 0;
         Dictionary<int, LineInfo> linesInfo = new();
+        Dictionary<int, int> numberOfCards = new();
         foreach (var line in inputLines)
         {
             if (string.IsNullOrEmpty(line))
@@ -25,13 +27,15 @@ internal class Day4 : IDay
             cardIndex++;
             var lineWithoutCard = Regex.Replace(line, pattern, string.Empty);
             var numbers = InputReader.ProcessStringLine(lineWithoutCard, '|');
-            var winningNumbers = numbers[0].Split(" ").Select(n => Convert.ToInt32(n.Trim())).ToHashSet();
-            var numbersYouHave = numbers[1].Split(" ").Select(n => Convert.ToInt32(n.Trim())).ToList();
+            var winningNumbers = numbers[0].Split(" ").Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => Convert.ToInt32(n.Trim())).ToHashSet();
+            var numbersYouHave = numbers[1].Split(" ").Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => Convert.ToInt32(n.Trim())).ToList();
             linesInfo[cardIndex] = new LineInfo(winningNumbers, numbersYouHave);
         }
 
-        foreach (var lineInfo in linesInfo.Values) 
+        foreach (var item in linesInfo) 
         {
+            var lineInfo = item.Value;
+            var cardNumber = item.Key;
             var matchingNumbers = new List<int>();
             foreach (var number in lineInfo.NumbersYouHave) 
             {
@@ -41,12 +45,23 @@ internal class Day4 : IDay
                 }
             }
             sum += CalculateCardPoints(matchingNumbers);
+            AddCard(numberOfCards, cardNumber);
+            for (int i = cardNumber + 1; i <= Math.Min(cardNumber + matchingNumbers.Count, cardIndex); i++)
+            {
+                for (var cardCopy = 0; cardCopy < numberOfCards[cardNumber]; cardCopy++)
+                {
+                    AddCard(numberOfCards, i);
+                }
+            }
         }
 
-        long sumGears = 0;
+        foreach (var card in numberOfCards.Keys)
+        {
+            totalCards += numberOfCards[card];
+        }
 
         Console.WriteLine($"{this.GetType().Name} result part 1: {sum}");
-        Console.WriteLine($"{this.GetType().Name} result part 2: {sumGears}");
+        Console.WriteLine($"{this.GetType().Name} result part 2: {totalCards}");
 
         static int CalculateCardPoints(List<int> matchingNumbers)
         {
@@ -56,15 +71,26 @@ internal class Day4 : IDay
                 sum = 1;
                 if (matchingNumbers.Count > 1) 
                 {
-                    sum++;
-                    for (int i = 2; i < matchingNumbers.Count; i++)
+                    for (int i = 1; i < matchingNumbers.Count; i++)
                     {
-                        sum += i * 2;
+                        sum *= 2;
                     }
                 }
             }
 
             return sum;
+        }
+
+        static void AddCard(Dictionary<int, int> numberOfCards, int cardIndex)
+        {
+            if (numberOfCards.ContainsKey(cardIndex))
+            {
+                numberOfCards[cardIndex] += 1;
+            }
+            else
+            {
+                numberOfCards[cardIndex] = 1;
+            }
         }
     }
 }
