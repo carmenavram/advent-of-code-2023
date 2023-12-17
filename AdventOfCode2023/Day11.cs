485c88db9f852304096a99e6f50ffd286b55ae37
carmenavram <carmene.avram@gmail.com>
2023-12-17 15:57:29 +0100

    refactor day 11

--- AdventOfCode2023/Day11.cs
+++ AdventOfCode2023/Day11.cs
@@ -2,151 +2,23 @@
 
 internal class Day11 : IDay
 {
-    private enum Direction
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    };
-
-    private static readonly IDictionary<Direction, Point> Diffs = new Dictionary<Direction, Point>()
-    {
-        [Direction.Up] = new Point(0, -1),
-        [Direction.Down] = new Point(0, 1),
-        [Direction.Left] = new Point(-1, 0),
-        [Direction.Right] = new Point(1, 0),
-    };
-
-    private const char EmptySpace = '.';
     private const char Galaxy = '#';
 
-    private record struct Point(int X, int Y) : IComparable<Point>
-    {
-        public int CompareTo(Point other)
-        {
-            if (X != other.X)
-            {
-                return X.CompareTo(other.X);
-            }
-            if (Y != other.Y)
-            {
-                return Y.CompareTo(other.Y);
-            }
-            return 0;
-        }
-    }
-
-    private record struct Tile(char Type, int GalaxyIndex, Dictionary<Direction, int> weightsPerDirection);
-
+    private record struct Point(int X, int Y);
+    
     public void Solve(IList<string?> inputLines)
     {
-        SolvePart(inputLines, 1);
-        SolvePart(inputLines, 2);
+        SolvePart(1, inputLines);
+        SolvePart(2, inputLines);
     }
-
-    private void SolvePart(IList<string?> inputLines, int part)
+   
+    private void SolvePart(int part, IList<string?> inputLines)
     {
         var expand = part == 1 ? 2 : 1000000;
-        var map = ReadMap(inputLines, expand);
-        var yLength = map.Max(m => m.Key.Y);
-        var xLength = map.Max(m => m.Key.X);
-        var galaxyPoints = map.Where(m => m.Value.GalaxyIndex > 0).ToDictionary(m => m.Value.GalaxyIndex, m => m.Key);
-
-        long sum = 0;
-        for (int i = 1; i <= galaxyPoints.Count; i++)
-        {
-            var paths = ShortestPaths(galaxyPoints[i], map);
-            for (int j = i + 1; j <= galaxyPoints.Count; j++)
-            {
-                sum += paths[galaxyPoints[j]].Sum(p => p.Weight);
-            }
-        }
-
-        Console.WriteLine($"{this.GetType().Name} result part {part}: {sum}");
-    }
-
-    private Dictionary<Point, List<(Point Coord, long Weight)>> ShortestPaths(Point start, Dictionary<Point, Tile> map)
-    {
-        var heap = new SortedSet<(long Weight, Point Coord)>();
-        var distances = new Dictionary<Point, long>();
-        var paths = new Dictionary<Point, List<(Point Coord, long Weight)>>();
-
-        heap.Add((0, start));
-        distances[start] = 0;
-        paths[start] = new List<(Point Coord, long Weight)>();
-
-        while (heap.Count > 0)
-        {
-            var node = heap.Min;
-            heap.Remove(node!);
-
-            Point currentNode = node.Coord;
-            foreach (var neighbor in GetNeighbours(currentNode, map))
-            {
-                long oldWeight;
-                distances.TryGetValue(neighbor.Coord, out oldWeight);
-                var newWeight = node.Weight + neighbor.Weight;
-
-                if (!distances.ContainsKey(neighbor.Coord) || newWeight < oldWeight)
-                {
-                    if (distances.ContainsKey(neighbor.Coord))
-                    {
-                        heap.Remove((oldWeight, neighbor.Coord));
-                    }
-
-                    distances[neighbor.Coord] = newWeight;
-                    heap.Add((newWeight, neighbor.Coord));
-
-                    var path = new List<(Point Coord, long Weight)>(paths[currentNode]);
-                    path.Add(neighbor);
-                    paths[neighbor.Coord] = path;
-                }
-            }
-        }
-
-        return paths;
-    }
-
-    private List<(Point Coord, long Weight)> GetNeighbours(Point currentPoint, Dictionary<Point, Tile> map)
-    {
-        var neighbours = new List<(Point Coord, long Weight)>();
-        foreach (var diff in Diffs)
-        {
-            var neigbourCoord = new Point(currentPoint.X + diff.Value.X, currentPoint.Y + diff.Value.Y);
-            if (map.ContainsKey(neigbourCoord))
-            {
-                neighbours.Add((neigbourCoord, map[neigbourCoord].weightsPerDirection[ReverseDirection(diff.Key)]));
-            }
-        }
-
-        return neighbours;
-    }
-
-    private static Direction ReverseDirection(Direction direction)
-    {
-        return direction switch
-        {
-            Direction.Up => Direction.Down,
-            Direction.Down => Direction.Up,
-            Direction.Left => Direction.Right,
-            Direction.Right => Direction.Left,
-            _ => throw new NotImplementedException(direction.ToString())
-        };
-    }
-
-    private static Dictionary<Point, Tile> ReadMap(IList<string?> inputLines, int expand)
-    {
-        Dictionary<Point, Tile> map = new();
         int x = 0;
         int y = 0;
-        int galaxyIndex = 1;
-        var previousLineIsEmpty = false;
-        var defaultWeightsPerDirection = new Dictionary<Direction, int>();
-        foreach (var direction in Diffs.Keys)
-        {
-            defaultWeightsPerDirection[direction] = 1;
-        }
+        Dictionary<int, Point> galaxies = new();
+        var galaxyIndex = 0;
         foreach (var line in inputLines)
         {
             if (string.IsNullOrWhiteSpace(line))
@@ -157,52 +29,49 @@ internal class Day11 : IDay
             x = 0;
             var tiles = line.ToCharArray();
             var lineIsEmpty = !tiles.Contains(Galaxy);
-            foreach (var tile in tiles)
+            if (lineIsEmpty)
             {
-                var coord = new Point(x, y);
-                var weightsPerDirection = new Dictionary<Direction, int>(defaultWeightsPerDirection);
-                if (lineIsEmpty)
-                {
-                    weightsPerDirection[Direction.Down] = expand;
-                }
-                if (previousLineIsEmpty)
-                {
-                    weightsPerDirection[Direction.Up] = expand;
-                }
-                map.Add(coord, new Tile(tile, tile == Galaxy ? galaxyIndex++ : 0, weightsPerDirection));
-                x++;
+                y += expand;
             }
-            previousLineIsEmpty = lineIsEmpty;
-            y++;
+            else
+            {
+                foreach (var tile in tiles)
+                {
+                    if (tile == Galaxy)
+                    {
+                        galaxies[++galaxyIndex] = new Point(x, y);
+                    }
+                    x++;
+                }
+                y++;
+            }
         }
 
-        var previousColumnIsEmpty = false;
-        for (int i = 0; i < x; i++)
+        var originalGalaxies = galaxies.ToDictionary(g => g.Key, g => g.Value);
+        for (int i = x - 1; i >= 0; i--)
         {
-            var isEmptyColumn = !map.Any(m => m.Key.X == i && m.Value.Type == Galaxy);
+            var isEmptyColumn = !galaxies.Any(g => g.Value.X == i);
             if (isEmptyColumn)
             {
-                for (int j = 0; j < y; j++)
+                for (int g = 1; g <= galaxyIndex; g++)
                 {
-                    var coord = new Point(i, j);
-                    var weightsPerDirection = map[coord].weightsPerDirection;
-                    weightsPerDirection[Direction.Right] = expand;
-                    map[coord] = new Tile(map[coord].Type, map[coord].GalaxyIndex, weightsPerDirection);
+                    if (originalGalaxies[g].X > i)
+                    {
+                        galaxies[g] = new Point(galaxies[g].X + expand - 1, galaxies[g].Y);
+                    }
                 }
             }
-            if (previousColumnIsEmpty)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    var coord = new Point(i, j);
-                    var weightsPerDirection = map[coord].weightsPerDirection;
-                    weightsPerDirection[Direction.Left] = expand;
-                    map[coord] = new Tile(map[coord].Type, map[coord].GalaxyIndex, weightsPerDirection);
-                }
-            }
-            previousColumnIsEmpty = isEmptyColumn;
         }
 
-        return map;
+        long sum = 0;
+        for (int i = 1; i <= galaxies.Count; i++)
+        {
+            for (int j = i + 1; j <= galaxies.Count; j++)
+            {
+                sum += Math.Abs(galaxies[i].X - galaxies[j].X) + Math.Abs(galaxies[i].Y - galaxies[j].Y);
+            }
+        }
+
+        Console.WriteLine($"{this.GetType().Name} result part {part}: {sum}");
     }
 }
